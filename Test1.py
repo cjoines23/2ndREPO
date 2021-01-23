8bf2a213f912440e026a580d23aea0505e476c16
cjoines23 <joinescolby23@gmail.com>
2021-01-23 13:09:24 -0500

    working json

--- Test1.py
+++ Test1.py
@@ -1,1 +1,8 @@
-print("Hey this is my program")
+import json
+
+jsonString = """{"people":[{"Id":"1","FirstName":"Colby","LastName":"Joines","Email":"joinescolby23@gmail.com"}]}"""
+
+
+jsonObj = json.loads(jsonString)
+
+print(jsonObj)
